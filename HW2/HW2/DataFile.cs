463f29750f9678460c25f1f75b20d85fa35beba4
Pavel-codes <fabelbutov79@gmail.com>
2023-04-07 15:17:27 +0300

    commit after gitignore

---
+++ HW2/HW2/DataFile.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+    internal class DataFile
+    {
+        private string FileName;
+        private DateTime LastUpdateTime;
+        private string Data;
+
+        public DataFile(string FileName, string Data)
+        {
+            setFileName(FileName);
+            LastUpdateTime = DateTime.Now;
+            this.Data = Data;
+        }
+        public DataFile() : this("someFile", "") { }
+        public DataFile(DataFile other)
+        {
+            FileName = "copy of " + other.FileName;
+            LastUpdateTime = DateTime.Now;
+        }
+
+        public string getFileName()
+        {
+            return FileName;
+        }
+
+        public void setFileName(string FileName)
+        {
+            char[] invalidChars = new char[] { '>', '<', '?', '/', ':', '*', '"', '\\' ,'|' };
+            if(invalidChars.Any(FileName.Contains))
+            {
+                while (invalidChars.Any(FileName.Contains))
+                {
+                    Console.WriteLine("A file name cannot contain and of the following characters : \\ / : * ? \" < > | ");
+                    Console.WriteLine("Please enter a new file name: ");
+                    FileName = Console.ReadLine();
+                }
+            }
+            this.FileName = FileName;
+        }
+        public string getData()
+        {
+            return this.Data;
+        }
+        public void setData(string Data)
+        {
+            this.Data = Data;
+        }
+        public void SetTime()
+        {
+            LastUpdateTime = DateTime.Now;
+        }
+        public DateTime GetTime()
+        {
+            return LastUpdateTime;
+        }
+        private float GetSize()
+        {
+            float size = 0;
+            const int KByte = 1024;
+            if(Data != null)
+            {
+                size = (float)Data.Length / KByte;
+            }
+            return size;
+        }
+        public void Dir()
+        {
+            Console.WriteLine($"{LastUpdateTime} {GetSize()}KB {FileName}.txt");
+        }
+    }
+} 
---
+++ HW2/HW2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            DataFile d1 = new DataFile();
+            DataFile d2 = new DataFile("myFile", "How much data do i need to write to get a significant number of characters for the function to show at least 0.1KB");
+            d1.Dir();
+            d2.Dir();
+            d1.setFileName("myFileRenewed");
+            d1.setData("Renamed the file and added some data");
+            d1.Dir();
+        }
+    }
+}
